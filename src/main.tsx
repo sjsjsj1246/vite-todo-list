2dd0243afa323ac6c9e1b4071074cabc8e9617bb
sjsjsj1246 <sjsjsj1246@gmail.com>
2024-08-10 01:11:15 +0900

    fix: top-level await 제거, build에 msw 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import axios from "axios";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
@@ -14,8 +14,6 @@ async function init() {
   }
 }
 
-await init();
-
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -24,8 +22,10 @@ const queryClient = new QueryClient({
   },
 });
 
-createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
-);
+init().then(() => {
+  createRoot(document.getElementById("root")!).render(
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  );
+});
